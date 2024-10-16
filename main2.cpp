e94e8a18947fe33e44d5547c78b813d5c94e6b3d
Jaimemuss <jaimefoxxpanera@gmail.com>
2024-10-16 01:01:16 -0700

    freak it

--- main2.cpp
+++ main2.cpp
@@ -10,15 +10,21 @@
 
 #include <cmath>
 
+#include <string>
+
 
 
  std::string player() {
 
 	std::string playerName;
+
+	Sleep(1000);
 	
 	std::cout << "What is your name ? ";
 
 	std::cin >> playerName;
+
+	Sleep(1000);
 
 	std::cout << "Nice to meet you " << playerName << std::endl;
 
@@ -27,7 +33,7 @@
 }
 
 
- static void welcome() {
+ void welcome() {
  
 	 std::cout << " Welcome to the Casino \n";
 
@@ -37,6 +43,8 @@
 	 std::cout << "We will be with you shortly \n";
 
 	 std::this_thread::sleep_for(std::chrono::seconds(3));
+
+	 return;
  
  }
 
@@ -60,16 +68,39 @@
 	 
 		 amI_Rich = true;
 
+		 Sleep(1000);
+
 		 std::cout << "Great you will be eligible to play at the high roller tables. \n";
 	 
 	 }
 	 else {
 		  amI_Rich = false;
+		  Sleep(1000);
 		 std::cout << "You are only eligible for standard play. Enjoy! \n";
 	 }
 
 	 return amI_Rich;
 
+	}
+
+	void highRollerMenu() {
+		
+		 
+
+			std::cout << " \n --Welcome to the high roller menu-- \n";
+
+			std::cout << "			 Check Balance \n";
+			
+			std::cout << "			   Games Menu \n";
+
+			std::cout << "		  Enter Standard Play \n";
+
+			std::cout << "			EXIT THE CASINO \n";
+
+
+
+		
+	
 	}
 
 	
@@ -79,7 +110,9 @@
 int main() 
  {
 	//welcome function self explanatory
-	void welcome();
+	 welcome();
+	
+	 Sleep(2000);
 
 	//function that gets the players name its static and cannot be called outside of this file might change
 	std::string playerName = player();
@@ -89,6 +122,23 @@ int main()
 
 	 //gets the money player has and puts it into the function to 
 	 bool isHighRoller = playerMoneyStatus(moneyHave);
+
+		
+	 while (isHighRoller == true) {
+
+		 bool running = true;
+
+		 while (running) {
+			 //code to clear the console display
+			 std::cout << "\033[2J\033[1;1H";
+				
+		 }
+	 
+		
+	 
+	 }
+
+
 
 
 
