a957c251f3e83c04efe973fc91d95f4dd3f0216d
jaimemuss <jaimefoxxpanera@gmail.com>
2024-10-15 20:40:32 -0700

    Yeahhhhhh

--- main2.cpp
+++ main2.cpp
@@ -10,11 +10,11 @@
 
 #include <cmath>
 
-#include <SFML/Graphics.hpp>
+
 
  std::string player() {
 
-	static std::string playerName;
+	std::string playerName;
 	
 	std::cout << "What is your name ? ";
 
@@ -27,7 +27,7 @@
 }
 
 
- void welcome() {
+ static void welcome() {
  
 	 std::cout << " Welcome to the Casino \n";
 
@@ -46,19 +46,49 @@
 
 	 std::cout << "How much money did you bring ? ";
 
-
-
-
+	 std::cin >> money;	
 		
+	 return money;
 
  }
 
- int  playerMoneyStatus(float &playerMoney) {
+	bool  playerMoneyStatus(float &playerMoney) {
 
+		bool amI_Rich = false;
+
+	 if (playerMoney > 10000) {
 	 
+		 amI_Rich = true;
+
+		 std::cout << "Great you will be eligible to play at the high roller tables. \n";
+	 
+	 }
+	 else {
+		  amI_Rich = false;
+		 std::cout << "You are only eligible for standard play. Enjoy! \n";
+	 }
+
+	 return amI_Rich;
+
+	}
+
  
 	
+	
+	
+int main() 
+ {
+	 
+	//calls to see how much money player has now reference this into the status to find out if hes a high roller
+	 float moneyHave = currentPlayerMoney();
 
+	 //gets the money player has and puts it into the function to 
+	 bool isHighRoller = playerMoneyStatus(moneyHave);
+
+
+
+	 return 0;
+ 
  }
 
 
